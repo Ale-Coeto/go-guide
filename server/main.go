3d966a28aa4ec0d6ed5a16838849cbe65c80259a
Alejandra Coeto <alecoeto@hotmail.com>
2025-10-04 18:09:08 -0700

    Added server code

---
+++ server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Request: /\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Request: /hello\n")
+	hasName := r.URL.Query().Has("name")
+	name := r.URL.Query().Get("name")
+	
+	if hasName {
+		io.WriteString(w, fmt.Sprintf("Hi %s! Welcome!\n", name))
+	} else {
+		io.WriteString(w, "Hello!\n")
+	}
+}
+
+func main() {
+	// Define main mux / server and handlers
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	// Run server
+	fmt.Println("Server starting on :3333")
+	err := http.ListenAndServe(":3333", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
